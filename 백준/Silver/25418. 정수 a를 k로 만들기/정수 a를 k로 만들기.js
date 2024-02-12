a1e6cfc0c3ce04c76717a383172a815d462ef1e5
Junho Cho <hiwhwnsgh@gmail.com>
2024-02-12 23:00:25 +0900

    Update 정수 a를 k로 만들기.js

--- 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.js
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.js
@@ -1,19 +1,16 @@
-function minOperationsToK(A, K) {
+function minCountToK(A, K) {
   let count = 0;
-  
   while (true) {
-      // K가 홀수이면 1을 빼고, 짝수이면 2로 나눈다
-      if(K===A){
-        console.log(count)
-        break
-      }
-      if(K % 2 === 0 && K>=A*2){
-        K /= 2
-      }
-      else{
-        K -=1
-      }
-      count++;
+    if(K === A) {
+      // A와 K가 같아지면 반복을 종료하고 현재까지의 연산 횟수를 반환한다.
+      return count;
+    }
+    if(K % 2 === 0 && K >= A * 2) {
+      K /= 2;
+    } else {
+      K -= 1;
+    }
+    count++;
   }
 }
 const readline = require("readline");
@@ -30,25 +27,5 @@ rl.on("line", (line) => {
 rl.on("close", () => {
     const A = input[0];
     const K = input[1];
-    // let queue = [[A,0]];
-    // let visited = new Set([A]);
-    // while(queue.length > 0){
-    //     const [current,count] = queue.shift();
-    //     if(current==K){
-    //       console.log(count);
-    //       break;
-    //     }
-    //     // 현재 값이 K보다 크면, 더 이상 더블링하지 않고 바로 다음 단계로 넘어감
-    //     if (current < K && !visited.has(current * 2)) {
-    //       queue.push([current * 2, count + 1]);
-    //       visited.add(current * 2);
-    //     }
-
-    //     if (!visited.has(current + 1)) {
-    //         queue.push([current + 1, count + 1]);
-    //         visited.add(current + 1);
-    //     }
-        
-    // }
-    minOperationsToK(A,K)
+    console.log(minCountToK(A, K)); // 출력: 151 또는 문제에 따라 다른 최소 연산 횟수
 });
